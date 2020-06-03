b44e462e2956d23f76a9d654d9a7b031babd50b3
weixiaoxing <weixiaoxing@meishubao.com>
2020-06-03 21:49:18 +0800

    update module

--- src/main/java/com/wxx/stream/apis/StreamForeach.java
+++ src/main/java/com/wxx/stream/apis/StreamForeach.java
@@ -2,9 +2,8 @@ package com.wxx.stream.apis;
 
 import com.wxx.domain.Student;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class StreamForeach {
 
@@ -13,14 +12,22 @@ public class StreamForeach {
 
     static {
         students.add(Student.builder().age(18).name("Weison").score(90).build());
+        students.add(Student.builder().age(18).name("Weison1").score(90).build());
         students.add(Student.builder().age(19).name("Evan").score(80).build());
+        students.add(Student.builder().age(19).name("Evan1").score(80).build());
         students.add(Student.builder().age(20).name("Jack").score(70).build());
         students.add(Student.builder().age(21).name("Luis").score(60).build());
+        students.add(Student.builder().age(21).name("Luis1").score(60).build());
     }
+
     public static void main(String[] args) {
         //1 forEach 迭代流中的元素
         System.out.println("-------------forEach 遍历stream中的元素-------------");
         strings.stream().forEach(System.out::println);
         students.stream().forEach(System.out::println);
+
+        Map<Integer, IntSummaryStatistics> collect = students.stream()
+                .collect(Collectors.groupingBy(Student::getAge, Collectors.summarizingInt(Student::getScore)));
+        System.out.println("=========" + collect);
     }
 }
