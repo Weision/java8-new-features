1c49fc89c70fd0b5609463a8317c8801e047b13c
WeisonWei <weixx3@126.com>
2020-08-17 01:48:01 +0800

    add test

--- src/test/java/com/weison/java8/AsyncTest.java
+++ src/test/java/com/weison/java8/AsyncTest.java
@@ -1,16 +1,25 @@
 package com.weison.java8;
 
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class AsyncTest {
 
     @Test
-    public void async() {
-        CompletableFuture.supplyAsync(() -> 1)
-                .thenApplyAsync(integer -> 2)
+    public void sync() {
+        CompletableFuture.supplyAsync(this::sleep)
                 .thenAccept(System.out::println);
-        System.out.println();
+        log.info("sync end");
+    }
+
+    @SneakyThrows
+    public Boolean sleep() {
+        TimeUnit.SECONDS.sleep(1L);
+        return Boolean.TRUE;
     }
 }
