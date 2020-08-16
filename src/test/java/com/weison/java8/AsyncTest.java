1722a0ba3a4f5429980081a53930b1952028cc9a
WeisonWei <weixx3@126.com>
2020-08-16 13:54:26 +0800

    init

---
+++ src/test/java/com/weison/java8/AsyncTest.java
@@ -0,0 +1,16 @@
+package com.weison.java8;
+
+import org.junit.Test;
+
+import java.util.concurrent.CompletableFuture;
+
+public class AsyncTest {
+
+    @Test
+    public void async() {
+        CompletableFuture.supplyAsync(() -> 1)
+                .thenApplyAsync(integer -> 2)
+                .thenAccept(System.out::println);
+        System.out.println();
+    }
+}
